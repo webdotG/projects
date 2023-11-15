9a8b46972a6aac6e75944aabef816ef65c3f74d7
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-16 00:36:50 +0300

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from './AllOver/Header/header'
 import Todo from './componentsList/Todo/ui/todo'
 // import useOutsideClick from './Hook/clickOutSide';
 import style from './App.module.scss'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
@@ -10,17 +10,28 @@ function App() {
   // const { ref } = useOutsideClick(false);
 
   const handleButtonClick = () => {
-    window.alert('111111111111111');
     setIsActive(!isActive);
    
   };
+  
+  const handleButtonClickClose = () => {
+    setIsActive(!isActive);
+    console.log('test')
+  };
 
+//   useEffect(() => {
+//     window.scroll(0, 0);
+// }, []);
   console.log(isActive)
 
   return (
     <div className={style.content_wrapper}>
       <Header />
       <ul className={style.item_list}>
+        <li className={style.item_wrapper}>
+        <h3>Grant Rental</h3>
+        <p>ui для аренды строительной техники</p>
+        </li>
         <li className={style.item_wrapper}>
           <h3>todo list</h3>
           <p>программа для создания задач</p>
@@ -29,10 +40,10 @@ function App() {
           {isActive && (
             <div className={`${isActive ? style.item__active : style.item}`}>  
                <button className={style.item_btn}
-            onClick={handleButtonClick}>закрыть</button>
+            onClick={handleButtonClickClose}>закрыть</button>
               <Todo />
               <button className={style.item_btn}
-            onClick={handleButtonClick}>закрыть</button>
+            onClick={handleButtonClickClose}>закрыть</button>
             </div>
           )}
         </li>
