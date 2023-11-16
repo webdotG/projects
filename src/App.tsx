3e0ff82ab370abf28698669f147ae36c3a947353
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-16 23:11:45 +0300

    add item favorite add programm + link

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,12 @@ function App() {
           <a className={style.item_link} href='https://webdotg.github.io/six_citise/' target="_blank">open</a>
         </li>
         <li className={style.item_wrapper}>
+          <h3 className={style.item_title}>Favorite add</h3>
+          <p className={style.item_description}>programm for adding favorite item</p>
+          <p className={style.item_stack}>React, Vite, ReduxToolkit, Axios, TS, Sass</p>
+          <a className={style.item_link} href='https://webdotg.github.io/favorite_add/' target="_blank">open</a>
+        </li>
+        <li className={style.item_wrapper}>
           <h3 className={style.item_title}>List "item/todo"</h3>
           <p className={style.item_description}>task add</p>
           <p className={style.item_stack}>React, Vite, AsyncThunk, Fetch</p>
