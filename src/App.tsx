76e582eaf8ae62cd9cb8f48be044334db96136c7
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-17 22:18:58 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,10 @@ function App() {
 
   const handleButtonClickClose = () => {
     setIsActive(!isActive);
+    window.scroll(0,0)
     console.log('test')
   };
 
-  //   useEffect(() => {
-  //     window.scroll(0, 0);
-  // }, []);
   console.log(isActive)
 
   return (
@@ -32,25 +30,25 @@ function App() {
         <li className={style.item_wrapper}>
           <h3 className={style.item_title}>Grant Rental</h3>
           <p className={style.item_description}>Site for rental of construction equipment</p>
-          <p className={style.item_stack}>React, Vite, ReduxToolkit, Axios, Firebase, TS, Sass</p>
+          <p className={style.item_stack}>TS, React, Vite, ReduxToolkit, Axios, Firebase, Sass</p>
           <a className={style.item_link} href='https://webdotg.github.io/Rental/' target="_blank">open</a>
         </li>
         <li className={style.item_wrapper}>
           <h3 className={style.item_title}>6sites booking hotel</h3>
-          <p className={style.item_description}>Site for booking hotel</p>
-          <p className={style.item_stack}>React, Vite, ReduxToolkit, Axios, TS, Sass</p>
+          <p className={style.item_description}>Site for booking hotel, at the moment only desktop version</p>
+          <p className={style.item_stack}>TS, React, Vite, ReduxToolkit, Axios, Sass</p>
           <a className={style.item_link} href='https://webdotg.github.io/six_citise/' target="_blank">open</a>
         </li>
         <li className={style.item_wrapper}>
           <h3 className={style.item_title}>Favorite add</h3>
-          <p className={style.item_description}>programm for adding favorite item</p>
-          <p className={style.item_stack}>React, Vite, ReduxToolkit, RTKQuery, TS, Sass</p>
+          <p className={style.item_description}>programm for adding favorite item, used an open source api Github</p>
+          <p className={style.item_stack}>TS, React, Vite, ReduxToolkit, RTKQuery, Sass</p>
           <a className={style.item_link} href='https://webdotg.github.io/favorite_add/' target="_blank">open</a>
         </li>
         <li className={style.item_wrapper}>
           <h3 className={style.item_title}>List item add</h3>
           <p className={style.item_description}>item AsyncThunk add</p>
-          <p className={style.item_stack}>React, Vite, AsyncThunk, Fetch</p>
+          <p className={style.item_stack}>TS, React, Vite, AsyncThunk, Fetch</p>
           <button className={style.item_link}
             onClick={handleButtonClick}>Open</button>
           {isActive && (
