0a52aa643b242b0c879dd066685c242edd869cbd
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-23 15:11:47 +0300

    j

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
           <h3 className={style.item_title}>RTK Query basic setup notes</h3>
           <p className={style.item_description}>cheat sheet</p>
           <p className={style.item_stack}>JS, React, Vite, RTK Query, JSON placeholder</p>
-          <a className={style.item_link} href='https://webdotg.github.io/RTKQuery_basicSetup/' target="_blank">open<p>open</p></a>
+          <a className={style.item_link} href='https://webdotg.github.io/RTKQuery_basicSetup/' target="_blank"><p>open</p></a>
         </li>
         <li className={style.item_wrapper}>
           <h3 className={style.item_title}>List item add</h3>
